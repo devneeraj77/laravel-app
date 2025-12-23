a7f911b3b9c7fac1b8337e87c427e9300898203d
devneeraj77 <dev.neerajrekwar@gmail.com>
2025-12-23 15:04:20 +0530

    reset --tag

--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -1,17 +1,9 @@
-@if(config('app.env') == 'production')
-    <!-- Google tag (gtag.js) -->
-    <!--
-        HOW TO USE:
-        1. Replace G-XXXXXXXXXX with your Google Analytics Measurement ID.
-        2. You can find your Measurement ID in your Google Analytics dashboard
-           under Admin > Data Streams > [Your Stream] > Measurement ID.
-    -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-B09KB964LQ"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-B09KB964LQ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-        gtag('config', 'G-B09KB964LQ');
-    </script>
-@endif
+  gtag('config', 'G-B09KB964LQ');
+</script>
